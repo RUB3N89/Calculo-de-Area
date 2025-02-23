1fecf54faec135e2c53b9f4f3cf6f111ba9e7e5d
RUB3N89 <158265042+RUB3N89@users.noreply.github.com>
2025-02-23 21:50:07 +0100

    Update AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -2,10 +2,10 @@ public class AreaCalculator {
 public staKc void main(String[] args) {
 System.out.println("Calculadora de área para diferentes figuras.");
 }
-public staKc double circleArea(double radius) {
+public static double circleArea(double radius) {
 return Math.PI * radius * radius;
 }
-public staKc double rectangleArea(double length, double width) {
+public static double rectangleArea(double length, double width) {
     return length * width;
     }
 }
